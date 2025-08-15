11bf1d133be725a4f315e2e9f0e84989ffe3c5f2
Yash Jain <yashjain07.yj@gmail.com>
2025-08-15 14:01:24 +0530

    Added The Service To Get The Response As Per User Query.

---
+++ src/services/chat.service.ts
@@ -0,0 +1,70 @@
+import { publicAxiosInstance } from "@/utils/instances/axiosInstance";
+import { AxiosError, AxiosResponse } from "axios";
+
+class ExtractErrorMessage{
+
+    private error: unknown;
+    private customErrorMessage: string;
+
+    constructor(error:unknown,customErrorMessage: string){
+
+        this.error = error;
+        this.customErrorMessage = customErrorMessage;
+
+    }
+
+
+    getErrorMessage(){
+
+        const err = this.error as AxiosError<{message: string}>;
+
+        return(
+
+            err?.message ||
+            err?.response?.data?.message ||
+            this.customErrorMessage
+        )
+
+    }
+
+}
+
+class ChatService{
+
+    static async sendYourQuery(userQuery: string): Promise<AxiosResponse | void>{
+
+        try {
+
+            const params = {
+                userQuery,
+            }
+
+            const response = await publicAxiosInstance.get("/chat", {params});
+
+            return response?.data;
+            
+        } catch (error) {
+
+            throw new Error(new ExtractErrorMessage(error, "Getting Error In sendYourQuery").getErrorMessage());
+            
+        }
+
+    }
+
+    static async getAllChatMessages(): Promise<AxiosResponse | void>{
+
+        try {
+            
+        } catch (error) {
+
+            console.log(error);
+
+            throw new Error(new ExtractErrorMessage(error, "Getting Error In GetAllChatMessages").getErrorMessage());
+            
+        }
+
+    }
+    
+}
+
+export default ChatService;
